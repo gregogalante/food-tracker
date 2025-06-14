f39b779ebd50c660f2387c519f896a30494f9e0c
Gregorio Galante <me@gregoriogalante.com>
2025-06-14 13:42:44 +0200

    Change temperature

--- api.php
+++ api.php
@@ -214,7 +214,7 @@ function callOpenAI($input) {
                 'content' => $input
             ]
         ],
-        'temperature' => 0.7
+        'temperature' => 0.25
     ];
     
     // Prepare curl request
